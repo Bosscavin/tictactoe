32f598b37169fc05ea1181f535883c49afb5e40e
Cavin Belga <cavinalbert@gmail.com>
2022-05-20 11:24:12 +0800

    added modal show and hide func

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,16 @@
+const playerConfigOverlay = document.getElementById('config-overlay');
+const backdrop = document.getElementById('backdrop');
+
 const editPlayerOneBtn = document.getElementById('edit-player-1-btn');
 const editPlayerTwoBtn = document.getElementById('edit-player-2-btn');
+const cancelConfigBtn = document.getElementById('cancel-config-btn');
+const confirmConfigBtn = document.getElementById('confirm-config-btn');
+
+
+editPlayerOneBtn.addEventListener('click', openPlayerConfig);
+editPlayerTwoBtn.addEventListener('click', openPlayerConfig);
+
+cancelConfigBtn.addEventListener('click', closePlayerConfig);
+
+
+backdrop.addEventListener('click', closePlayerConfig);
---
+++ scripts/config.js
@@ -0,0 +1,9 @@
+const openPlayerConfig = () => {
+    playerConfigOverlay.style.display = 'block';
+    backdrop.style.display = 'block';
+};
+
+const closePlayerConfig = () => {
+    playerConfigOverlay.style.display = 'none';
+    backdrop.style.display = 'none';
+}
